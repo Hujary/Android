b4555a240f1e44f5fba21d311bf9d7a041c4c68a
OsgFreeze <noah@pilchowski.net>
2023-07-10 20:38:15 +0200

    Design 1.0

--- app/src/main/java/com/example/navigationsbar/Activitys/FilteredGameList/DetailedGameInformationActivity.java
+++ app/src/main/java/com/example/navigationsbar/Activitys/FilteredGameList/DetailedGameInformationActivity.java
@@ -1,6 +1,11 @@
 package com.example.navigationsbar.Activitys.FilteredGameList;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.style.RelativeSizeSpan;
+import android.text.style.StyleSpan;
 import android.view.View;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
@@ -35,18 +40,33 @@ public class DetailedGameInformationActivity extends AppCompatActivity {
             String difficulty = extras.getString("difficulty");
             String rules = extras.getString("rules");
 
+
+
                 // Verwendet die übergebenen Werte, um die Ansichten in Ihrem Layout zu aktualisieren
+
             TextView titleTextView = findViewById(R.id.gameNameTextView);
-            titleTextView.setText(title);
+            String emoji = "\uD83C\uDF7B";
+            titleTextView.setText(title + " " + emoji);
+
+                // Custom Formatierung
 
             TextView playerNumberTextView = findViewById(R.id.playerCountTextView);
-            playerNumberTextView.setText("Benötigte Spieler: " + playerNumberMin + " - " + playerNumberMax);
+            String TextSpieler = "Benötigte Spieler: " + playerNumberMin + " - " + playerNumberMax;
+            SpannableString spannableString1 = new SpannableString(TextSpieler);
+            spannableString1.setSpan(new StyleSpan(Typeface.BOLD), 0, 17, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+            playerNumberTextView.setText(spannableString1);
 
             TextView playTimeTextView = findViewById(R.id.gameDurationTextView);
-            playTimeTextView.setText("Spielzeit: " + playTimeMin + "min - " + playTimeMax + "min");
+            String TextZeit = "Spielzeit: " + playTimeMin + "min - " + playTimeMax + "min";
+            SpannableString spannableString2 = new SpannableString(TextZeit);
+            spannableString2.setSpan(new StyleSpan(Typeface.BOLD), 0, 9, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+            playTimeTextView.setText(TextZeit);
 
             TextView difficultyTextView = findViewById(R.id.difficultyTextView);
-            difficultyTextView.setText("Schwierigkeit: " + difficulty);
+            String TextSchwierigkeit = "Schwierigkeit: " + difficulty;
+            SpannableString spannableString3 = new SpannableString(TextSchwierigkeit);
+            spannableString3.setSpan(new StyleSpan(Typeface.BOLD), 0, 13, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+            difficultyTextView.setText(TextSchwierigkeit);
 
             TextView rulesTextView = findViewById(R.id.rulesTextView);
             rulesTextView.setText(rules);
